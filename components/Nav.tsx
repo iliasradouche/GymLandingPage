26039d0390c54d7f367f903816b353a75c5ab27a
iliasradouche <iliasradouche2@gmail.com>
2024-07-04 20:13:03 +0100

    Navbar changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,11 +3,10 @@ import { Link as ScrollLink } from "react-scroll";
 const links = [
   { name: "home", target: "home", offset: -100 },
   { name: "about", target: "about", offset: -80 },
-  { name: "class", target: "class", offset: -80 },
+
   { name: "team", target: "team", offset: 0 },
   { name: "prices", target: "prices", offset: -40 },
-  { name: "testimonial", target: "testimonial", offset: 0 },
-  { name: "blog", target: "blog", offset: 0 },
+
   { name: "contact", target: "contact", offset: -100 },
 ];
 
